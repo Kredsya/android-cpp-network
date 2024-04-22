6e62cd0fab90776064cb1bba06036a89899459e2
Kredsya <do901328@gmail.com>
2024-04-23 02:02:41 +0900

    Update: readme about interface-list / Modify: build-libpcap.sh with sysroot64 or 32

--- README.md
+++ README.md
@@ -13,3 +13,9 @@ Our organization : [cppdroid](https://github.com/cppdroid) (private organization
 I tried to cross compile with CMake (between not only armeabi-v7a and arm64-v8a, but also C and CPP).
 
 It was big help to understand CMake and Android Cross Compiling with NDK.
+
+## [interface-list](interface-list)
+
+To do this, I need to cross compile libpcap in arm64-v8a and armeabi-v7a.
+
+While doing this, I understand -DCMAKE_INSTALL_PREFIX, concept of CMake's root directory, and difference of static and shared linking.
--- interface-list/build-libpcap.sh
+++ interface-list/build-libpcap.sh
@@ -3,10 +3,10 @@ tar -xf libpcap-1.10.4
 cd libpcap-1.10.4
 mkdir build
 cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=/home/kredsya/study/android-cpp-network/interface-list/AndroidCrossCompile.cmake \
-      -DDISABLE_DBUS=True \
-      -DDISABLE_DPDK=True \
-      -DCMAKE_INSTALL_PREFIX=/home/kredsya/study/android-cpp-network/interface-list/sysroot \
-      -DBUILD_SHARED_LIBS=False \
-      ..
+cmake -DCMAKE_TOOLCHAIN_FILE=../../android-cpp-network/etc/AndroidCrossCompile.cmake \
+	-DDISABLE_DBUS=True \
+	-DDISABLE_DPDK=True \
+	-DCMAKE_INSTALL_PREFIX=../../libpcapCross/sysroot64 \
+	-DBUILD_SHARED_LIBS=False \
+	.. \
 make install
